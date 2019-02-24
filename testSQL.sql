25696f48b12a55044ab5fcb1751a8d4716647ab4
Stanislav Dalinsky <s.dalinsky@yandex.ru>
2019-02-24 20:31:06 +0300

    ch.1

---
+++ testSQL.sql
@@ -0,0 +1,24 @@
+create temp table users(id bigserial, group_id bigint);
+insert into
+   users(group_id) 
+values (1), (1), (1), (2), (1), (3);
+
+with temp_table as 
+(
+   select
+      - id + row_number() over (partition by group_id) as help_id,
+      id,
+      group_id 
+   from
+      users 
+)
+select
+   min(id) as min_id,
+   min(group_id) as group_id,
+   count(id) as count 
+from
+   temp_table 
+group by
+   help_id 
+order by
+   min_id
